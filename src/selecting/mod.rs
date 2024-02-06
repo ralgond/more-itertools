af96edf9378f6e0d3ce615723391d800513193aa
Teng Huang <ht201509@163.com>
2024-02-06 11:08:34 +0800

    add function only.version.1

--- src/selecting/mod.rs
+++ src/selecting/mod.rs
@@ -2,3 +2,4 @@ pub mod first;
 pub mod last;
 pub mod nth_or_last;
 pub mod one;
+pub mod only;
---
+++ src/selecting/only.rs
@@ -0,0 +1,62 @@
+use crate::error::Error;
+use crate::error;
+use crate::look_ahead_back::spy::spy;
+
+pub fn only<I>(iterable: I, default: Option<I::Item>) -> Result<I::Item, Error> 
+where 
+I: IntoIterator,
+I::Item: Clone
+{
+    let result;
+    let ret = spy(iterable, 2);
+    match ret {
+        None => {
+            match default {
+                None => { return Err(error::value_error("too short and default is None".to_string())); }
+                Some(v) => { return Ok(v); }
+            }
+        },
+        Some(vec) => {
+            if vec.len() > 1 {
+                return Err(error::value_error("too long".to_string()));
+            } else if vec.len() == 0 {
+                match default {
+                    None => { return Err(error::value_error("too short and default is None".to_string())); }
+                    Some(v) => { return Ok(v); }
+                }
+            } else {
+                result = vec[0].clone();
+            }
+        }
+    }
+
+    return Ok(result);
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let v1: Vec<String>= Vec::new();
+        let ret1 = only(v1, Some("missing".to_string()));
+        match ret1 {
+            Err(_) => { assert!(false) },
+            Ok(_) => { assert!(true); }
+        }
+
+        let v1: Vec<String>= vec!["too".to_string(), "many".to_string()];
+        let ret1 = only(v1, Some("missing".to_string()));
+        match ret1 {
+            Err(e) => { assert_eq!(*(e.message().unwrap()), String::from("too long")); },
+            Ok(_) => { assert!(false); }
+        }
+
+        let v1: Vec<String>= vec!["too".to_string()];
+        let ret1 = only(v1, Some("missing".to_string()));
+        match ret1 {
+            Err(_) => { assert!(false); },
+            Ok(v) => { assert_eq!(v, "too".to_string()); }
+        }
+    }
+}
