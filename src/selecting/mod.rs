47d1a93543524cc5a7b33a991e80242707443f37
Teng Huang <ht201509@163.com>
2024-02-06 10:14:38 +0800

    add function one.version.1

--- src/selecting/mod.rs
+++ src/selecting/mod.rs
@@ -1,3 +1,4 @@
 pub mod first;
 pub mod last;
 pub mod nth_or_last;
+pub mod one;
---
+++ src/selecting/one.rs
@@ -0,0 +1,54 @@
+use crate::error::Error;
+use crate::error;
+use crate::look_ahead_back::spy::spy;
+
+pub fn one<I>(iterable: I) -> Result<I::Item, Error> 
+where 
+I: IntoIterator,
+I::Item: Clone
+{
+    let result;
+    let ret = spy(iterable, 2);
+    match ret {
+        None => { return Err(error::value_error("too short".to_string())); },
+        Some(vec) => {
+            if vec.len() > 1 {
+                return Err(error::value_error("too long".to_string()));
+            } else if vec.len() == 0 {
+                return Err(error::value_error("too short".to_string()));
+            } else {
+                result = vec[0].clone();
+            }
+        }
+    }
+
+    return Ok(result);
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let v1: Vec<String>= Vec::new();
+        let ret1 = one(v1);
+        match ret1 {
+            Err(e) => { assert_eq!(*(e.message().unwrap()), String::from("too short")); },
+            Ok(_) => { assert!(false); }
+        }
+
+        let v1: Vec<String>= vec!["too".to_string(), "many".to_string()];
+        let ret1 = one(v1);
+        match ret1 {
+            Err(e) => { assert_eq!(*(e.message().unwrap()), String::from("too long")); },
+            Ok(_) => { assert!(false); }
+        }
+
+        let v1: Vec<String>= vec!["too".to_string()];
+        let ret1 = one(v1);
+        match ret1 {
+            Err(_) => { assert!(false); },
+            Ok(v) => { assert_eq!(v, "too".to_string()); }
+        }
+    }
+}
