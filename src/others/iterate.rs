12500dff471f15ad87efdd25ebc8e49c0d044512
Teng Huang <ht201509@163.com>
2024-02-19 08:17:08 +0800

    add islice in unittest.

--- src/others/iterate.rs
+++ src/others/iterate.rs
@@ -48,6 +48,8 @@ pub fn iterate<T>(func: fn(&T) -> T, start: T) -> Iterate<T>
 
 #[cfg(test)]
 mod tests {
+    use crate::{islice::islice, utils::extract_value_from_result_vec};
+
     use super::*;
 
     #[test]
@@ -58,5 +60,13 @@ mod tests {
         assert_eq!(Some(4), it.next());
         assert_eq!(Some(8), it.next());
         assert_eq!(Some(16), it.next());
+    }
+
+    #[test]
+    fn test2() {
+        let i = islice(iterate(|x: &i32| { x * 2 }, 1), 0, 10, 1);
+        let j = extract_value_from_result_vec(i.collect::<Vec<_>>());
+        // println!("{:?}", j.0);
+        assert_eq!(vec![1, 2, 4, 8, 16, 32, 64, 128, 256, 512], j.0);
     }
 }
