4c90850ef1bbef6b9eab4df31be78c17fd1ccb28
Teng Huang <ht201509@163.com>
2024-02-20 08:54:35 +0800

    add function locate.version.2

--- src/others/locate.rs
+++ src/others/locate.rs
@@ -1,35 +1,40 @@
+use crate::error::Error;
+use crate::error;
+
 #[derive(Debug,Clone)]
 pub struct Locate<T> {
     array: Vec<T>,
     query: Vec<T>,
-    offset: usize,
-    window_size: usize
+    offset: usize
 }
 
 impl<T> Iterator for Locate<T> 
 where 
 T: Clone + PartialEq
 {
-    type Item = usize;
+    type Item = Result<usize, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.window_size != self.query.len() {
-            return None;
-        }
 
         loop {
-            if self.array.len() - self.offset < self.window_size {
+            let of_add = self.query.len().overflowing_add(self.offset);
+            if of_add.1 {
+                //overflow
+                return Some(Err(error::overflow_error("offset overflow".to_string())));
+            }
+
+            if self.array.len() < self.query.len() + self.offset {
                 return None;
             }
     
-            let slice = &self.array[self.offset..self.offset+self.window_size];
+            let slice = &self.array[self.offset..self.offset+self.query.len()];
 
             if slice.len() == self.query.len() && self.query.starts_with(slice) {
                 let ret_offset = self.offset;
     
                 self.offset += 1;
         
-                return Some(ret_offset);
+                return Some(Ok(ret_offset));
             } else {
                 self.offset += 1;
             }
@@ -39,14 +44,12 @@ T: Clone + PartialEq
 
 pub fn locate<T>(
                     array: Vec<T>,
-                    query: Vec<T>,
-                    window_size: usize) -> Locate<T>
+                    query: Vec<T>) -> Locate<T>
 {
     Locate {
         array: array,
         query: query,
-        offset: 0,
-        window_size: window_size
+        offset: 0
     }
 }
 
@@ -57,19 +60,26 @@ mod tests {
 
     #[test]
     fn test1() {
-        let mut l = locate(vec![1,1,1,1,1], vec![1,1,1], 3);
-        assert_eq!(Some(0), l.next());
-        assert_eq!(Some(1), l.next());
-        assert_eq!(Some(2), l.next());
+        let mut l = locate(vec![1,1,1,1,1], vec![1,1,1]);
+        assert_eq!(Some(Ok(0)), l.next());
+        assert_eq!(Some(Ok(1)), l.next());
+        assert_eq!(Some(Ok(2)), l.next());
         assert_eq!(None, l.next());
         assert_eq!(None, l.next());
 
-        let mut l = locate(vec![1,1,1,1,1], vec![1], 1);
-        assert_eq!(Some(0), l.next());
-        assert_eq!(Some(1), l.next());
-        assert_eq!(Some(2), l.next());
-        assert_eq!(Some(3), l.next());
-        assert_eq!(Some(4), l.next());
+        let mut l = locate(vec![1,1,1,1,1], vec![1]);
+        assert_eq!(Some(Ok(0)), l.next());
+        assert_eq!(Some(Ok(1)), l.next());
+        assert_eq!(Some(Ok(2)), l.next());
+        assert_eq!(Some(Ok(3)), l.next());
+        assert_eq!(Some(Ok(4)), l.next());
+        assert_eq!(None, l.next());
+
+
+        let mut l = locate(vec![0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3], vec![1,2,3]);
+        assert_eq!(Some(Ok(1)), l.next());
+        assert_eq!(Some(Ok(5)), l.next());
+        assert_eq!(Some(Ok(9)), l.next());
         assert_eq!(None, l.next());
     }
 }
