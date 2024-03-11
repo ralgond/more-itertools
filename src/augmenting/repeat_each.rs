9ad01e0c293cb1b4400844b5375150ee06252d19
Teng Huang <ht201509@163.com>
2024-03-11 20:49:27 +0800

    Refactor: use Result<T,Error>. update function repeat_each.

--- src/augmenting/repeat_each.rs
+++ src/augmenting/repeat_each.rs
@@ -1,16 +1,15 @@
-use std::collections::VecDeque;
-
+use crate::error:: Error;
 
 pub struct RepeatEach<T> 
 where
 T: Clone
 {
-    iter: Box<dyn Iterator<Item = T>>,
+    // cl: CacheLast<T>,
+    iter: Box<dyn Iterator<Item = Result<T,Error>>>,
     n: usize,
-    item_buffer: VecDeque<T>,
     emit_count: usize,
     iter_finished: bool,
-    emit_finished : bool
+    last_item: Option<Result<T, Error>>
 }
 
 
@@ -18,70 +17,76 @@ impl<T> Iterator for RepeatEach<T>
 where
 T: Clone
 {
-    type Item = T;
+    type Item = Result<T, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if self.emit_finished {
+            if self.iter_finished {
                 return None;
             }
 
-            if !self.iter_finished && self.item_buffer.len() == 0 {
-                let _next = self.iter.next();
-                match _next {
-                    None => {
-                        self.iter_finished = true;
-                        self.emit_finished = true;
-                        return None;
-                    },
-                    Some(v) => {
-                        self.item_buffer.push_back(v);
-                        //assert!(self.item_buffer.len() == 1)
+            if self.emit_count < self.n {
+                self.emit_count += 1;
+                return self.last_item.clone();
+            } else {
+                self.last_item = self.iter.next();
+                if self.last_item.is_none() {
+                    self.emit_count = 0;
+                    self.iter_finished = true;
+                    return None;
+                } else {
+                    match self.last_item.as_ref().unwrap() {
+                        Ok(_) => {
+                            self.emit_count = 0;
+                            continue;
+                        },
+                        Err(err_v) => {
+                            self.iter_finished = true;
+                            return Some(Err(err_v.clone()));
+                        }
                     }
                 }
-            }
-
-            if self.emit_count == self.n {
-                self.item_buffer.pop_front();
-                self.emit_count = 0;
-                continue;
-            } else {
-                self.emit_count += 1;
-                return Some(self.item_buffer.front_mut().unwrap().clone());
             }
         }
     }
 }
 
-pub fn repeat_each<T>(iter: Box<dyn Iterator<Item=T>>, n: usize) -> Box<dyn Iterator<Item=T>>
+pub fn repeat_each<T>(mut iter: Box<dyn Iterator<Item=Result<T, Error>>>, n: usize) -> Box<dyn Iterator<Item=Result<T, Error>>>
 where
 T: Clone + 'static
 {
+    let last_item = iter.next();
+
     Box::new(RepeatEach {
         iter,
         n,
-        item_buffer: VecDeque::new(),
         emit_count: 0,
         iter_finished: false,
-        emit_finished: false
+        last_item 
     })
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::itertools::iter::iter_from_vec;
+    use crate::{error, utils::{extract_value_from_result_vec, generate_okok_iterator, generate_okokerr_iterator}};
 
     use super::*;
 
     #[test]
     fn test1() {
         let v = vec![1,2,3];
-        let ret = repeat_each(iter_from_vec(v), 3).collect::<Vec<_>>();
-        assert_eq!(vec![1, 1, 1, 2, 2, 2, 3, 3, 3], ret);
+        let ret = repeat_each(generate_okok_iterator(v), 3).collect::<Vec<_>>();
+        assert_eq!(vec![1, 1, 1, 2, 2, 2, 3, 3, 3], extract_value_from_result_vec(ret).0);
 
         let v = vec![1,2,3];
-        let ret = repeat_each(iter_from_vec(v), 0).collect::<Vec<_>>();
+        let ret = repeat_each(generate_okok_iterator(v), 0).collect::<Vec<_>>();
         assert_eq!(0, ret.len());
+
+        let v = vec![1,2,3];
+        let ret = repeat_each(generate_okokerr_iterator(v, error::overflow_error("[test]".to_string())), 3).collect::<Vec<_>>();
+        let ret2 = extract_value_from_result_vec(ret);
+        assert_eq!(vec![1, 1, 1, 2, 2, 2, 3, 3, 3], ret2.0);
+        assert_eq!(error::Kind::OverflowError, ret2.1.unwrap().kind());
     }
 }
